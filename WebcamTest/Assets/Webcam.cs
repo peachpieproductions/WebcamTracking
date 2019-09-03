af30b9168ade324867771e82a8e3b07d8cf6e830
Ben James <benjames.im@gmail.com>
2019-09-03 17:20:27 -0400

    Bull following player

--- WebcamTest/Assets/Webcam.cs
+++ WebcamTest/Assets/Webcam.cs
@@ -10,6 +10,9 @@ public class Webcam : MonoBehaviour
     public RawImage rawImage;
     public Vector2 spaceExtents;
     public ParticleSystem particleSys;
+    public Transform bullNeckBone;
+    public Vector2 averageCoords;
+    public Transform testObj;
 
     WebCamTexture webcamTex;
     Color32[] previousFrame;
@@ -36,6 +39,43 @@ public class Webcam : MonoBehaviour
         StartCoroutine(CompareImageData());
     }
 
+    private void Update() {
+
+        Vector3 towardObjectFromHead = (testObj.position + Vector3.back * 20) - bullNeckBone.position;
+        towardObjectFromHead.x *= -1;
+        towardObjectFromHead *= .5f;
+        bullNeckBone.rotation = Quaternion.Lerp(bullNeckBone.rotation, Quaternion.LookRotation(towardObjectFromHead, transform.up), Time.deltaTime * 4f);
+        //bullNeckBone.rotation = Quaternion.LookRotation(towardObjectFromHead, transform.up);
+
+
+        // Store the current head rotation since we will be resetting it
+        /*Quaternion currentLocalRotation = headBone.localRotation;
+        // Reset the head rotation so our world to local space transformation will use the head's zero rotation. 
+        // Note: Quaternion.Identity is the quaternion equivalent of "zero"
+        headBone.localRotation = Quaternion.identity;
+
+        Vector3 targetWorldLookDir = target.position - headBone.position;
+        Vector3 targetLocalLookDir = headBone.InverseTransformDirection(targetWorldLookDir);
+
+        // Apply angle limit
+        targetLocalLookDir = Vector3.RotateTowards(
+            Vector3.forward,
+            targetLocalLookDir,
+            Mathf.Deg2Rad * headMaxTurnAngle, // Note we multiply by Mathf.Deg2Rad here to convert degrees to radians
+            0 // We don't care about the length here, so we leave it at zero
+        );
+
+        // Get the local rotation by using LookRotation on a local directional vector
+        Quaternion targetLocalRotation = Quaternion.LookRotation(targetLocalLookDir, Vector3.up);
+
+        // Apply smoothing
+        headBone.localRotation = Quaternion.Slerp(
+            currentLocalRotation,
+            targetLocalRotation,
+            1 - Mathf.Exp(-headTrackingSpeed * Time.deltaTime)
+        );*/
+    }
+
     public IEnumerator CompareImageData() {
 
         previousFrame = webcamTex.GetPixels32();
@@ -53,15 +93,17 @@ public class Webcam : MonoBehaviour
 
                 if (diff > .3f) changedPixelsList.Add(i);
             }
-            Debug.Log(changedPixelsList.Count);
+            //Debug.Log(changedPixelsList.Count);
 
             int j = 0;
+            Vector2 coordinateSum = Vector2.zero;
             foreach(int pixelId in changedPixelsList) {
                 j++;
                 int yCoord = Mathf.FloorToInt(pixelId / webcamTex.width);
                 int xCoord = Mathf.FloorToInt(pixelId - yCoord * webcamTex.width);
                 Vector2 coords = new Vector2(xCoord, yCoord);
-                if (j > 100 && j < 110) Debug.Log(pixelId + ", " + xCoord + ", " + yCoord);
+                coordinateSum += coords;
+                //if (j > 100 && j < 110) Debug.Log(pixelId + ", " + xCoord + ", " + yCoord);
                 if (Random.value < .02f) {
                     particleSys.transform.localPosition = new Vector3((coords.x / webcamTex.width) * spaceExtents.x, (coords.y / webcamTex.height) * spaceExtents.y, 0);
                     var main = particleSys.main;
@@ -71,6 +113,10 @@ public class Webcam : MonoBehaviour
                 }
                 //Debug.Log(coords);
             }
+
+            averageCoords = coordinateSum / changedPixelsList.Count;
+
+            testObj.localPosition = Vector3.Lerp(testObj.localPosition, new Vector3 ((averageCoords.x / webcamTex.width) * spaceExtents.x, (averageCoords.y / webcamTex.height) * spaceExtents.y, 0), Time.deltaTime * 50f);
 
             hue = (hue + Time.deltaTime * 1f) % 1f;
 
